7524f974677c93b2d5c5f84b43ac53386f250f20
Peter Heise <peter.heise@knorr-bremse.com>
2022-03-29 13:49:57 +0200

    Added bash script.

---
+++ airbyte-integrations/connectors/source-mariadb/build_source.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# First build the container
+docker build . -t airbyte/source-example-python:dev
+
+# Then use the following commands to run it
+docker run --rm airbyte/source-example-python:dev spec
+docker run --rm -v $(pwd)/secrets:/secrets airbyte/source-example-python:dev check --config /secrets/config.json
+#docker run --rm -v $(pwd)/secrets:/secrets airbyte/source-example-python:dev discover --config /secrets/config.json
+#docker run --rm -v $(pwd)/secrets:/secrets -v $(pwd)/sample_files:/sample_files airbyte/source-example-python:dev read --config /secrets/config.json --catalog /sample_files/configured_catalog.json
